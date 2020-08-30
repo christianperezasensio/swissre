62c02b07f03695f009b426a2bb596beab0e046a1
christianperezasensio <christian.perez.asensio@gmail.com>
2020-08-30 14:37:05 +0100

    Added utility to read files from classpath for testing

--- src/test/java/com/swissre/assessment/EuroPortfolioTest.java
+++ src/test/java/com/swissre/assessment/EuroPortfolioTest.java
@@ -1,4 +1,22 @@
 package com.swissre.assessment;
 
-public class EuroPortfolioTest {
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static com.swissre.assessment.utils.TestUtils.readData;
+
+class EuroPortfolioTest {
+
+    @Test
+    void readFile() {
+        String file = "/payload/portfolio.txt";
+        try {
+            String payload = readData(file);
+            Assert.assertTrue(payload.contains("BTC=10"));
+        } catch (IOException e) {
+            Assert.fail("Error loading file " + file);
+        }
+    }
 }
